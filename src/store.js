76aaeb0cb3618ec564c7ef7e64cf08d115e35485
Run Chen <rzhen12@gmail.com>
2025-05-21 12:06:15 -0400

    implement conference event planner with redux

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReucer from './avSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReucer,
+    meals: mealsReducer
   },
 });
